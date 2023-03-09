2bdf36eab5483801d4e89410df9a737d8db19569
manprog <hamzamusaddiq@yahoo.com>
2023-03-09 23:02:44 +0100

    Removed create app funtion

--- app/__init__.py
+++ app/__init__.py
@@ -29,43 +29,31 @@ jobs_context = [
             ]
 
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_mapping(
+    SECRET_KEY='dev',
+    DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+)
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+# ensure the instance folder exists
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass
 
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World! Just testing'
-    
-    @app.route('/')
-    def index():
-        return render_template('index.html', jobs = jobs_context)
-    
-    @app.route('/api/jobs')
-    def jobs():
-        return jsonify(jobs_context)
+# a simple page that says hello
+@app.route('/hello')
+def hello():
+    return 'Hello, World! Just testing'
 
-    return app
-    
+@app.route('/')
+def index():
+    return render_template('index.html', jobs = jobs_context)
+
+@app.route('/api/jobs')
+def jobs():
+    return jsonify(jobs_context)
 
 if __name__ == '__main__':
-    app = create_app()
     app.run(debug=True)
