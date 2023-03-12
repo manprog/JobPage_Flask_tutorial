59507d08685c7b4a8fbb5c9420eb9541b5f5025f
manprog <hamzamusaddiq@yahoo.com>
2023-03-12 23:18:05 +0100

    Added form and data entry into database

--- app/__init__.py
+++ app/__init__.py
@@ -1,34 +1,8 @@
 import os
 
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, abort, request
 
-from .database import load_jobs_from_db
-
-# jobs_context = [
-#             {
-#                 'id': 1,
-#                 'title': "Data Analyst",
-#                 'location': 'Bremen',
-#                 'salary': '50k'
-#             },
-#             {
-#                 'id': 2,
-#                 'title': "Python Programmer",
-#                 'location': 'Paris',
-#                 'salary': '60k'
-#             },
-#             {
-#                 'id': 1,
-#                 'title': "Frontend Developer",
-#                 'location': 'Berlin',
-#                 'salary': '45k'
-#             },
-#             {
-#                 'id': 1,
-#                 'title': "Backend Developer",
-#                 'location': 'Multan'
-#             }
-#             ]
+from .database import load_jobs_from_db, load_single_job_from_db, add_application_into_db
 
 
 app = Flask(__name__, instance_relative_config=True)
@@ -49,9 +23,28 @@ except OSError:
 def index():
     return render_template('index.html', jobs = load_jobs_from_db())
 
+@app.route('/job/<id>')
+def single_job_page(id):
+    return render_template('job_description.html', job=load_single_job_from_db(id))
+
+@app.route('/job/<id>/apply/', methods=['post'])
+def apply_to_job(id):
+    data = request.form  # Use request.form for post method
+    job=load_single_job_from_db(id)
+    add_application_into_db(job_id=id, data=data)
+    return render_template('form_submitted.html', data=data)
+
 @app.route('/api/jobs')
-def jobs():
+def jobs_api():
     return jsonify(load_jobs_from_db())
+
+@app.route('/api/job/<id>')
+def single_job_api(id):
+    job = load_single_job_from_db(id)
+    if job is not None:
+        return jsonify(job)
+    else:
+        abort(404)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- app/database.py
+++ app/database.py
@@ -17,3 +17,23 @@ def load_jobs_from_db():
         results = conn.execute(text("SELECT * FROM jobs")).all()
         jobs_list = [row._mapping for row in results]
     return jobs_list
+
+def load_single_job_from_db(id):
+    with engine.connect() as conn:
+        results = conn.execute(text("SELECT * FROM jobs where id={}".format(id))).all()
+    if len(results) > 0:
+        job_list = [row._mapping for row in results]
+        return dict(job_list[0])
+    else:
+        return None
+    
+def add_application_into_db(job_id, data):
+    full_name = data['name'] 
+    email = data['email']
+    country = data['country']
+    degree = data['degree']
+    linkedin_url = data['linkedin']
+
+    with engine.connect() as conn:
+        insert_str = "INSERT INTO applications (job_id, full_name, email, country, degree, linkedin_url) VALUES ('{job_id}', '{full_name}', '{email}', '{country}', '{degree}', '{linkedin_url}');".format(job_id=job_id, full_name=full_name, email=email, country=country, degree=degree, linkedin_url=linkedin_url)
+        conn.execute(text(insert_str))
